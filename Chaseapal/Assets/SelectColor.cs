76e15b4603d497be902bb6f351d6659aaa2985c2
Knase23 <jesper.uddefors.95@gmail.com>
2018-11-06 16:28:02 +0100

    Okay i will commit, not suicide, just a commit

---
+++ Chaseapal/Assets/SelectColor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectColor : MonoBehaviour {
+
+    public string color = "blue";
+
+    public GameObject blueDino;
+    public GameObject greenDino;
+    public GameObject redDino;
+    public GameObject yellowDino;
+
+
+    // Use this for initialization
+    void Start () {
+
+        color.ToLower();
+        switch (color)
+        {
+            case "blue":
+               Instantiate(blueDino,gameObject.transform);
+                break;
+            case "green":
+                Instantiate(greenDino, gameObject.transform);
+                break;
+            case "red":
+                Instantiate(redDino, gameObject.transform);
+                break;
+            case "yellow":
+                Instantiate(yellowDino, gameObject.transform);
+                break;
+            default:
+               Instantiate(blueDino, gameObject.transform);
+                break;
+        }
+        
+	}
+}
