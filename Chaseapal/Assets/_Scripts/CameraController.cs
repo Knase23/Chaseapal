dae8c3eb1956bef665e99562953834ee29390dc8
oscaroders <oscar.oders@gmail.com>
2018-11-07 16:57:34 +0100

    cameracontroller victory screen

---
+++ Chaseapal/Assets/_Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Camera cam2P;
+    public Camera cam3P;
+    public Camera cam4P;
+
+    // Use this for initialization
+    void Start () {
+        if (2P){ // how to know how many palyers??
+            cam2P.enabled = true;
+            cam3P.enabled = false;
+            cam4P.enabled = false;
+        } else if (3P){
+            cam2P.enabled = true;
+            cam3P.enabled = false;
+            cam4P.enabled = false;
+        } else if (4P){
+            cam2P.enabled = true;
+            cam3P.enabled = false;
+            cam4P.enabled = false;
+        }
+    }
+
+}
